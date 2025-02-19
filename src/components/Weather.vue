ae8e39e3f3eabcaaa3ecc3f98e5ae5eb5e9e5ac5
Doggytrop <a22311120@uthermosillo.edu.mx>
2025-02-19 09:29:21 -0700

    guardado

---
+++ src/components/Weather.vue
@@ -0,0 +1,17 @@
+<template>
+<h3>La temperatura actual es: {{ WeatherStore.temperatura }}°</h3>
+
+
+</template>
+<script setup>
+import {useWeather} from '@/composables/useWeather';
+import {onMounted} from 'vue';
+import{useWeatherStore} from '@/stores/weatherStore'
+
+const WeatherStore = useWeatherStore();
+
+onMounted(()=>{
+    useWeather();
+});
+</script>
+<style scoped></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,4 +1,5 @@
 <template>
+<Header></Header>
 <div class="container">
     <Sidebar></Sidebar>
     <div class="content">
@@ -8,13 +9,16 @@
 </template>
 <script setup>
 import Sidebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
 </script>
 <style scoped>
 .container {
     display: flex;
+    margin-top: 10px;
+    padding-top: 50px;
 }
 .content {
+    
     flex: 1;
-    padding: 20px;
 }
 </style>
