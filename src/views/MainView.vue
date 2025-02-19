f6c7e2173ba6d63f7fd4d691f8e4beb9c8c0b99d
Doggytrop <a22311120@uthermosillo.edu.mx>
2025-02-19 09:42:15 -0700

    cambios

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,10 @@
 <template>
-<Header></Header>
+<Header :mensaje="texto"></Header>
 <div class="container">
     <Sidebar></Sidebar>
     <div class="content">
+        <input type="text" v-model="texto">
+        {{ texto }}
         <router-view></router-view>
     </div>
 </div>
@@ -10,6 +12,9 @@
 <script setup>
 import Sidebar from '@/components/Sidebar.vue';
 import Header from '@/components/Header.vue';
+import {ref} from 'vue';
+
+const texto = ref('')
 </script>
 <style scoped>
 .container {
